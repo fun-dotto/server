84e26d94b04fe7551c6e5c9e9dc0693a736d0642
Hikaru Saito <beretta0602@gmail.com>
2026-09-12 13:35:24 +0900

    FK外した

--- internal/shared/model/calendar_date.go
+++ internal/shared/model/calendar_date.go
@@ -3,8 +3,7 @@ package model
 type CalendarDate struct {
 	Common
 
-	ServiceID     string    `gorm:"not null;index"`
-	Calendar      *Calendar `gorm:"belongsTo;foreignKey:ServiceID;references:ServiceID"`
-	Date          string    `gorm:"not null;index"`
-	ExceptionType int       `gorm:"not null"`
+	ServiceID     string `gorm:"not null;index"`
+	Date          string `gorm:"not null;index"`
+	ExceptionType int    `gorm:"not null"`
 }
--- internal/shared/model/trip.go
+++ internal/shared/model/trip.go
@@ -3,10 +3,9 @@ package model
 type Trip struct {
 	Common
 
-	TripID      string    `gorm:"not null;uniqueIndex"`
-	RouteID     string    `gorm:"not null;index"`
-	Route       *Route    `gorm:"belongsTo;foreignKey:RouteID;references:RouteID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	ServiceID   string    `gorm:"not null;index"`
-	Calendar    *Calendar `gorm:"belongsTo;foreignKey:ServiceID;references:ServiceID"`
-	DirectionID int       `gorm:"not null"`
+	TripID      string `gorm:"not null;uniqueIndex"`
+	RouteID     string `gorm:"not null;index"`
+	Route       *Route `gorm:"belongsTo;foreignKey:RouteID;references:RouteID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	ServiceID   string `gorm:"not null;index"`
+	DirectionID int    `gorm:"not null"`
 }
