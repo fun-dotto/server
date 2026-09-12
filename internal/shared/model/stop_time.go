10f9cf64138333e884d8c405e63e681b9fd28c67
Hikaru Saito <beretta0602@gmail.com>
2026-09-12 13:19:41 +0900

    コメント追加

--- internal/shared/model/stop_time.go
+++ internal/shared/model/stop_time.go
@@ -4,6 +4,7 @@ type StopTime struct {
 	Common
 
 	TripID        string `gorm:"not null;index"`
+	// belongsTo は非公式だが gorm.io/gorm の schema/relationship.go で実際に処理されるタグなので削除しない
 	Trip          *Trip  `gorm:"belongsTo;foreignKey:TripID;references:TripID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	ArrivalTime   string `gorm:"not null"`
 	DepartureTime string `gorm:"not null"`
