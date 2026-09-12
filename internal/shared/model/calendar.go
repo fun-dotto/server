9b6debb32d090fb8f649e156194d3ea5b4cdb733
Hikaru Saito <beretta0602@gmail.com>
2026-09-12 20:19:56 +0900

    Dateをtime型に

--- internal/shared/model/calendar.go
+++ internal/shared/model/calendar.go
@@ -1,16 +1,18 @@
 package model
 
+import "time"
+
 type Calendar struct {
 	Common
 
-	ServiceID string `gorm:"not null;uniqueIndex"`
-	Monday    int    `gorm:"not null"`
-	Tuesday   int    `gorm:"not null"`
-	Wednesday int    `gorm:"not null"`
-	Thursday  int    `gorm:"not null"`
-	Friday    int    `gorm:"not null"`
-	Saturday  int    `gorm:"not null"`
-	Sunday    int    `gorm:"not null"`
-	StartDate string `gorm:"not null"`
-	EndDate   string `gorm:"not null"`
+	ServiceID string    `gorm:"not null;uniqueIndex"`
+	Monday    int       `gorm:"not null"`
+	Tuesday   int       `gorm:"not null"`
+	Wednesday int       `gorm:"not null"`
+	Thursday  int       `gorm:"not null"`
+	Friday    int       `gorm:"not null"`
+	Saturday  int       `gorm:"not null"`
+	Sunday    int       `gorm:"not null"`
+	StartDate time.Time `gorm:"type:date;not null"`
+	EndDate   time.Time `gorm:"type:date;not null"`
 }
--- internal/shared/model/calendar_date.go
+++ internal/shared/model/calendar_date.go
@@ -1,9 +1,11 @@
 package model
 
+import "time"
+
 type CalendarDate struct {
 	Common
 
-	ServiceID     string `gorm:"not null;index"`
-	Date          string `gorm:"not null;index"`
-	ExceptionType int    `gorm:"not null"`
+	ServiceID     string    `gorm:"not null;index"`
+	Date          time.Time `gorm:"type:date;not null;index"`
+	ExceptionType int       `gorm:"not null"`
 }
