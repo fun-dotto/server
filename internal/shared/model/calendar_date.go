393c5334324b310291f46c229c1255746d8a35e0
Hikaru Saito <beretta0602@gmail.com>
2026-09-18 22:37:15 +0900

    ユニーク制約追加

--- internal/shared/model/calendar_date.go
+++ internal/shared/model/calendar_date.go
@@ -5,7 +5,7 @@ import "time"
 type CalendarDate struct {
 	Common
 
-	ServiceID     string    `gorm:"not null;index"`
-	Date          time.Time `gorm:"type:date;not null;index"`
+	ServiceID     string    `gorm:"not null;uniqueIndex:idx_calendar_dates_service_date"`
+	Date          time.Time `gorm:"type:date;not null;index;uniqueIndex:idx_calendar_dates_service_date"`
 	ExceptionType int       `gorm:"not null;check:exception_type IN (1,2)"`
 }
